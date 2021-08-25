497ea569bf32213be722004296ef44bcd253c084
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-08-25 01:30:50 -0500

    Elimination of default Vue "Hello world" view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 import ListQuote from '@/components/Quote/ListQuote'
 
 Vue.use(Router)
@@ -9,11 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/quotes',
       name: 'ListQuote',
       component: ListQuote
     }
