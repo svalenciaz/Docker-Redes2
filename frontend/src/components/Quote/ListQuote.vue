452ce31f414409d7ccfa527ea076be37bb09e8d2
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-08-25 16:29:50 -0500

    New quote added

--- frontend/src/components/Quote/ListQuote.vue
+++ frontend/src/components/Quote/ListQuote.vue
@@ -2,6 +2,46 @@
     <div class="container">
         <div class="row">
             <div class="col text-left">
+                <h2>Crear nueva cita</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <img src="" alt="Escudo Universidad Nacional de Colombia">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-9">
+                <div class="card">
+                    <div class="card-body">
+                        <form @submit='onSubmit'>
+                            <div class="form-group row">
+                                <label for="author" class="col-sm-2 col-form-label">Autor</label>
+                                <div class="col-sm-6">
+                                    <input type="text" placeholder="Autor" name="author" class="form-control" v-model.trim="form.author">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="phrase" class="col-sm-2 col-form-label">Frase</label>
+                                <div class="col-sm-6">
+                                    <textarea type="text" placeholder="Frase" name="phrase" class="form-control" rows="3" v-model.trim="form.phrase"></textarea>
+                                </div>
+                            </div>
+                            <div class="rows">
+                                <div class="col text-left">
+                                    <b-button type='submit' variant="primary">Agregar nueva cita</b-button>
+                                    <b-button type='submit' class="btn-large-space" :to="{name: 'ListQuote'}">Cancelar</b-button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-left">
                 <h2>Citas</h2>
                 <div class="col-md-12">
                     <b-table striped hover :items="quotes" :fields="fields">
@@ -22,7 +62,11 @@ export default {
                 { key: 'author', label: 'Autor'},
                 { key: 'phrase', label: 'Frase'},
             ],
-            quotes: []
+            quotes: [],
+            form:{
+                author: '',
+                phrase: '',
+            }
         }
     },
     methods: {
@@ -36,7 +80,21 @@ export default {
                 console.log(error)
             })
 
-        }
+        },
+        onSubmit (event) {
+            event.preventDefault()
+
+            const path = 'http://127.0.0.1:8000/api/v1.0/quotes/'
+            axios.post(path, this.form).then((response) =>{
+                this.form.author = response.data.author
+                this.form.phrase = response.data.phrase
+                this.getQuotes();
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            
+        },
     },
     created(){
         this.getQuotes();
