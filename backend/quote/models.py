21e0dbe2bad7c500f242dd65ebff3549edb4daa4
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-08-28 01:38:16 -0500

    New project

--- backend/quote/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Quote(models.Model):
-    author = models.CharField(max_length=50)
-    phrase = models.TextField()
--- backend/quote/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers, urlpatterns
-
-from .viewsets import QuoteViewSet
-
-router = routers.SimpleRouter()
-router.register('quotes', QuoteViewSet)
-
-urlpatterns = router.urls
